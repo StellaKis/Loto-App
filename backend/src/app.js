6f927b46e457bbdbd27c517ad2293baa97a8375f
StellaKis <sk53957@fer.hr>
2025-10-25 19:03:47 +0200

    Poruka

--- backend/src/app.js
+++ backend/src/app.js
@@ -27,7 +27,7 @@ const config = {
   authRequired: false,          
   auth0Logout: true,
   secret: process.env.AUTH0_CLIENT_SECRET,
-  baseURL: 'https://loto-app-1-qyz9.onrender.com',
+  baseURL: 'https://loto-app-b6qy.onrender.com',
   clientID: process.env.AUTH0_CLIENT_ID,
   issuerBaseURL: `https://dev-48knub1ve523umrc.us.auth0.com`
 };
